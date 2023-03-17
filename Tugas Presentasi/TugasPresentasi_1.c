6206de0fb0a8a852ec17ed6c5a288662612f192f
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-17 14:16:32 +0700

    add input

--- Tugas Presentasi/TugasPresentasi_1.c
+++ Tugas Presentasi/TugasPresentasi_1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 1001
 
 void swap(int *a, int *b);
 int checkSorting(int arr[], int n);
 void show(int arr[], size_t length);
+void input(int arr[], size_t length);
 
 int main(){
-    int i, j, diff;
+    int i, j, diff, length;
     int output = 1;
-    int arr[] = {2, 1, 4, 3, 5};
-    int length = sizeof(arr) / sizeof(arr[0]);
+    int arr[SIZE];
+    printf("Enter the length of your array: ");
+    scanf("%d", &length);
+    input(arr, length);
     show(arr, length);
 
     output = checkSorting(arr, length);
@@ -55,4 +59,11 @@ void show(int arr[], size_t length){
         printf("%d ", arr[i]);
     }
     puts("");
+}
+
+void input(int arr[], size_t length){
+    for(int i = 0; i < length; i++){
+        printf("Enter index %d: ", i);
+        scanf("%d", &arr[i]);
+    }
 }
