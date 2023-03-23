7316d42cbee4caff12973d8f1190b0b4849a8dd5
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-23 12:11:36 +0700

    finish sorting and searching

--- TugasKelompok_3.c
+++ TugasKelompok_3.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
+#define SIZE 10
 
-int search(int arr[], int N, int x);
 void swap(int *a, int *b);
-void selectionSort(int arr[], size_t length);
-void insertionSort(int arr[], size_t length);
+void show(int arr[], int length);
+int linearSearch(int arr[], int N, int x);
+int binarySearch(int arr[], int l, int r, int target);
+void selectionSort(int arr[], int length);
+void insertionSort(int arr[], int length);
 
 
 int main(){
 	int data,i;
-	int array[10]={1,3,6,10,15,16,20,25,37,50};
-	
-	printf("Array: ");
-	for(i=0;i<10;i++){
-		printf("%d ",array[i]);
-	}
+	int array[SIZE] = {0};
+    int linearResult, binaryResult;
 
-    // printf("Array yang telah terurut menggunakan insertion sort: ");
-    // printf("Array yang telah terurut menggunakan selection sort: ");
+    puts("Masukkan 10 angka!");
+    for(i = 0; i < SIZE; i++){
+        printf("Index ke-%d: ", i+1);
+        scanf("%d", &array[i]);
+    }
 	
-	printf("\nMasukkan data yang dicari:");
+	printf("\nArray: \n");
+	show(array, SIZE);
+
+    insertionSort(array, SIZE);
+    printf("\nArray yang telah terurut menggunakan insertion sort:\n");
+    show(array, SIZE);
+    selectionSort(array, SIZE);
+    printf("\nArray yang telah terurut menggunakan selection sort:\n");
+    show(array, SIZE);
+	
+	printf("\nMasukkan data yang dicari: ");
 	scanf("%d",&data);
-	
-	int N = sizeof(array) / sizeof(array[0]);
  
-    int result = search(array, N, data);
-    (result == -1)
-        ? printf("Elemen tidak terdapat dalam array")
-        : printf("Elemen berada pada indeks ke-%d", result);
-    return 0;
-}
+    linearResult = linearSearch(array, 10, data);
+    if(linearResult == -1) printf("Elemen tidak terdapat dalam array dengan linear search\n");
+    else printf("Elemen berada pada indeks ke-%d dengan linear search\n", linearResult);
+    binaryResult = binarySearch(array, 0, 10, data);
+    if(binaryResult == -1) printf("Elemen tidak terdapat dalam array dengan binary search\n");
+    else printf("Elemen berada pada indeks ke-%d dengan binary search\n", binaryResult);
 
-int search(int arr[], int N, int x){
-    int i;
-    for (i = 0; i < N; i++)
-        if (arr[i] == x)
-            return i;
-    return -1;
+    return 0;
 }
 
 void swap(int *a, int *b){
@@ -44,20 +49,47 @@ void swap(int *a, int *b){
     *b = temp;
 }
 
-void selectionSort(int arr[], size_t length){
+void show(int arr[], int length){
+    int i;
+    for(i=0;i<length;i++){
+		printf("%d, ",arr[i]);
+	}
+}
+
+int linearSearch(int arr[], int N, int x){
+    int i;
+    for (i = 0; i < N; i++){
+        if (arr[i] == x) return i;
+    }
+    return -1;
+}
+
+int binarySearch(int arr[], int l, int r, int target){
+    int mid;
+
+    while(r >= l){
+        mid = l + (r - l) / 2;
+        if(target == arr[mid]) return mid;
+        else if (target < arr[mid]) r = mid-1;
+        else l = mid+1;
+    }
+    return -1;
+}
+
+void selectionSort(int arr[], int length){
     int i, j, min_index;
     for(i = 0; i < length - 1; i++){
         min_index = i;
-        for(j = 0; j < length; j++){
+        for(j = i+1; j < length; j++){
             if(arr[j] < arr[min_index]){
                 min_index = j;
             }
         }
-        swap(&arr[min_index], &arr[j]);
+        swap(&arr[min_index], &arr[i]); 
     }
 }
 
-void insertionSort(int arr[], size_t length){
+void insertionSort(int arr[], int length){
     int i, j, key;
     for(i = 1; i < length; i++){
         key = arr[i];
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+);
+    else printf("Elemen berada pada indeks ke-%d dengan linear search\n", linearResult);
