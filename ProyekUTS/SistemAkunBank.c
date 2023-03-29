06bfa343aaf9d7f4ca513cb3f2e9ad7e6c673099
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-29 21:38:16 +0700

    add midterm project

---
+++ ProyekUTS/SistemAkunBank.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+TUGAS PROYEK KELOMPOK UTS:
+SISTEM AKUN BANK
+
+Anggota: 
+Aulia Anugrah A
+Muhammad Abrisam
+Naufal Rusyda Santoso
+
+FITUR:
+* Bisa menyimpan informasi Biodata pembuat rekening (termasuk password) dan melakukan pendaftaran rekening jika belum punya
+* Sistem bisa mengurutkan berdasarkan jumlah saldo, nama atau nomor rekening (ada pilihan admin dan nasabah di awal program)
+* Bisa cek informasi saldo rekening
+* Bisa transfer rekening ke tujuan
+
+*/
+
+int main(){
+
+    return 0;
+}
