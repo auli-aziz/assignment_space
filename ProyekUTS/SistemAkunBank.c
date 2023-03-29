84545479b3787bee757adc7576e69f306fd49f68
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-29 21:40:58 +0700

    tweak features desc

--- ProyekUTS/SistemAkunBank.c
+++ ProyekUTS/SistemAkunBank.c
@@ -12,10 +12,11 @@ Muhammad Abrisam
 Naufal Rusyda Santoso
 
 FITUR:
-* Bisa menyimpan informasi Biodata pembuat rekening (termasuk password) dan melakukan pendaftaran rekening jika belum punya
-* Sistem bisa mengurutkan berdasarkan jumlah saldo, nama atau nomor rekening (ada pilihan admin dan nasabah di awal program)
-* Bisa cek informasi saldo rekening
-* Bisa transfer rekening ke tujuan
+* Bisa menyimpan informasi Biodata pembuat rekening (termasuk password) dan melakukan pendaftaran rekening jika belum punya (STRUCT)
+* Sistem bisa mengurutkan berdasarkan jumlah saldo, nama atau nomor rekening (ada pilihan admin dan nasabah di awal program) (S0RTING)
+* Bisa cek informasi saldo rekening (FILE HANDLING)
+* Bisa transfer rekening ke tujuan (SEARCHING)
+* Bisa cek bunga jika sudah menjadi nasabah selama beberapa lama (RECURSION)
 
 */
 
