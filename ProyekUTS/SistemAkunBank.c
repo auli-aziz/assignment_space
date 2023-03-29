39ca2a5cc81cdd0ece20c769e62822cc98818eb4
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-29 23:42:22 +0700

    add struct

--- ProyekUTS/SistemAkunBank.c
+++ ProyekUTS/SistemAkunBank.c
@@ -3,13 +3,14 @@
 #include <string.h>
 
 /*
-TUGAS PROYEK KELOMPOK UTS:
-SISTEM AKUN BANK
+PEMROGRAMAN LANJUT 01
+TUGAS PROYEK KELOMPOK TENGAH SEMESTER
+JUDUL: SISTEM AKUN BANK
 
 Anggota: 
-Aulia Anugrah A
-Muhammad Abrisam
-Naufal Rusyda Santoso
+Muhammad Abrisam Cahyo Juhartono | 2206026050
+Aulia Anugrah Aziz | 2206059364
+Naufal Rusyda Santosa | 2206813353
 
 FITUR:
 * Bisa menyimpan informasi Biodata pembuat rekening (termasuk password) dan melakukan pendaftaran rekening jika belum punya (STRUCT)
@@ -18,7 +19,28 @@ FITUR:
 * Bisa transfer rekening ke tujuan (SEARCHING)
 * Bisa cek bunga jika sudah menjadi nasabah selama beberapa lama (RECURSION)
 
+TEKNIK KOMPUTER
+UNIVERSITAS INDONESIA
 */
+
+typedef struct Akun {
+    char namaDepan[25];
+    char namaBelakang[25];
+    char username[50];
+    char namaIbu[50];
+    char alamat[100];
+    int tanggal, bulan, tahun;
+} Akun;
+
+typedef struct Pass {
+    char password[100];
+} Pass;
+
+typedef struct Transfer {
+    char tujuan[100];
+    char asal[100];
+    long int uang;
+} Transfer;
 
 int main(){
 
