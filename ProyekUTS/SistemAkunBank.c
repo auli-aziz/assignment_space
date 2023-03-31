d8256350be6ad629b246563566ca040e08319290
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-31 21:32:44 +0700

    tambah pembagian tugas

--- ProyekUTS/SistemAkunBank.c
+++ ProyekUTS/SistemAkunBank.c
@@ -19,6 +19,11 @@ FITUR:
 * Bisa cek informasi saldo rekening (FILE HANDLING)
 * Bisa transfer rekening ke tujuan (SEARCHING)
 * Bisa cek bunga jika sudah menjadi nasabah selama beberapa lama (RECURSION)
+
+PEMBAGIAN TUGAS:
+* Ari: login
+* Aziz: bikin akun, admin, nasabah
+* Naufal: cek bunga, sorting
 *********************************************
 TEKNIK KOMPUTER
 UNIVERSITAS INDONESIA
@@ -45,7 +50,11 @@ typedef struct Transfer {
     long int uang;
 } Transfer;
 
+void admin(int menu);
+void nasabah(int menu, struct Akun data);
 void bikinAkun();
+void login();
+void transfer();
 
 int main(){
     int menu, opsiUser = 0;
@@ -56,31 +65,9 @@ int main(){
     printf("MASUK SEBAGAI\n1. NASABAH\n2. ADMIN\nPILIHAN: ");
     scanf("%d", &opsiUser);
     if(opsiUser == 1){
-        system("CLS");
-        printf("SILAKAN PILIH JENIS MENU YANG TERSEDIA\n");
-        printf("1. MEMBUAT AKUN BARU\n2. SUDAH MEMPUNYAI AKUN\nPILIHAN: ");
-        scanf("%d", &menu);
-        switch(menu){
-            case 1:
-            system("CLS");
-            bikinAkun();
-            break;
-            case 2:
-            break;
-            default:
-            printf("PILIHAN TIDAK DITEMUKAN\n");
-        }
+        nasabah(menu);
     } else if(opsiUser == 2){
-        system("CLS");
-        printf("ANDA MASUK SEBAGAI ADMIN\nSILAKAN MASUKKAN PIN ADMIN: ");
-        scanf("%d", &data.passAdmin);
-        if(data.passAdmin == 000){
-            printf("1. SORT\n2. SEARCH REKENING\nPILIHAN: ");
-            scanf("%d", &menu);
-        } else{
-            printf("PASSWORD SALAH! \n");
-            exit(0);
-        }
+        admin(menu, data);
     } else{
         printf("MENU TIDAK TERSEDIA \n");
     }
@@ -88,21 +75,58 @@ int main(){
     return 0;
 }
 
+void nasabah(int menu){
+    system("CLS");
+    printf("SILAKAN PILIH JENIS MENU YANG TERSEDIA\n");
+    printf("1. MEMBUAT AKUN BARU\n2. SUDAH MEMPUNYAI AKUN\nPILIHAN: ");
+    scanf("%d", &menu);
+    switch(menu){
+        case 1:
+        bikinAkun();
+        break;
+        case 2:
+
+        break;
+        default:
+        printf("PILIHAN TIDAK DITEMUKAN\n");
+    }
+}
+
+void nasabah(int menu, struct Pin data){
+    system("CLS");
+    printf("ANDA MASUK SEBAGAI ADMIN\nSILAKAN MASUKKAN PIN ADMIN: ");
+    scanf("%d", &data.passAdmin);
+    if(data.passAdmin == 000){
+        printf("1. SORT\n2. SEARCH REKENING\nPILIHAN: ");
+        scanf("%d", &menu);
+    } else{
+        printf("PASSWORD SALAH! \n");
+    }
+}
+
 void bikinAkun(){
     FILE *fptr;
     Akun data;
+    system("CLS");
     printf("MENU PEMBUATAN AKUN BARU BANK JAGO\n");
     // sleep(1);
     printf("NAMA DEPAN: ");
     getchar();
     fgets(data.namaDepan, sizeof(data.namaDepan), stdin);
-    data.namaDepan[strcspn(data.namaDepan, "\n")] = '\0'; //removes newline
+    data.namaDepan[strcspn(data.namaDepan, "\n")] = '\0'; //removes new line
     printf("NAMA BELAKANG: ");
     fgets(data.namaBelakang, sizeof(data.namaBelakang), stdin);
     printf("NAMA IBU KANDUNG: ");
     fgets(data.namaIbu, sizeof(data.namaIbu), stdin);
     printf("ALAMAT DOMISILI: ");
     fgets(data.alamat, sizeof(data.alamat), stdin);
+    printf("MASUKKAN TANGGAL LAHIR: ");
+    scnaf("%d", &data.tanggal);
+    getchar();
+    scnaf("%d", &data.bulan);
+    getchar();
+    scnaf("%d", &data.tahun);
+
     printf("USERNAME (DAPAT BERUPA ANGKA DAN HURUF SAJA): ");
     fgets(data.username, sizeof(data.username), stdin);
 
